9883c3c1dbe13d8b74548da4bc31f01de82b804f
SJ <suresh@vicarasolutions.com>
2022-08-10 00:25:45 -0400

    See issue #4: Added a logger to make debugging easier - Fixed a rootPath bug

---
+++ src/logging.ts
@@ -0,0 +1,37 @@
+import * as vscode from "vscode";
+
+export type LOG_LEVEL = "DEBUG" | "INFO" | "WARN" | "ERROR" | "FATAL";
+
+class Logger {
+  private channel = vscode.window.createOutputChannel("Suspenders");
+
+  log(message: string, level?: LOG_LEVEL) {
+    // TODO: Wrap in a level check
+    if (level) {
+        this.channel.append(`[${level}] `)
+    }
+    this.channel.appendLine(`${message}`);
+  }
+
+  debug(message: string) {
+    this.log(message, "DEBUG");
+  }
+  info(message: string) {
+    this.log(message, "INFO");
+  }
+  warning(message: string) {
+    this.log(message, "WARN");
+  }
+  error(message: string) {
+    this.log(message, "ERROR");
+  }
+  fatal(message: string) {
+    this.log(message, "FATAL");
+  }
+
+  show() {
+    this.channel.show(true);
+  }
+}
+
+export const logger = new Logger();
