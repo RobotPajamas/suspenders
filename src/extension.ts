2add58d608c5d1326785a17365732df6353c7fe6
sj <suresh@vicarasolutions.com>
2024-05-09 22:50:15 -0400

    See issue #27: Suuuuuper early proof-of-concept using pyright with a custom stub path to a builtins file

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,31 @@
 import * as vscode from "vscode";
 import * as proc from "child_process";
+import * as path from "path";
 import { TargetsProvider, SourceRootsProvider } from "./treeviews";
 import { logger } from "./logging";
 import { CodeLensProvider } from "./codelens";
 import { TestsProvider } from "./test-explorer";
 import { getPantsExecutable } from "./configuration";
+import {
+  CancellationToken,
+  ConfigurationParams,
+  ConfigurationRequest,
+  DidChangeConfigurationNotification,
+  LanguageClient,
+  LanguageClientOptions,
+  ResponseError,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
 // TODO: Destructure vscode imports
-
+let lspClient: LanguageClient;
 // let disposables: vscode.Disposable[] = [];
-
-function runGoalOnAllTargets(name: string, cwd?: string): void {
-  logger.show();
-
-  const options: proc.SpawnOptions = {
-    cwd,
-  };
-  const subprocess = proc.spawn(getPantsExecutable(), [name, "::"], options);
-  subprocess.stdout?.on("data", (data) => logger.log(data.toString()));
-  subprocess.stderr?.on("data", (data) => logger.log(data.toString()));
-}
 
 const extensionName = "robotpajamas.vscode-suspenders";
 const extensionVersion = "0.0.2";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   logger.info(`Extension name: ${extensionName}`);
   logger.info(`Extension version: ${extensionVersion}`);
 
@@ -66,8 +67,8 @@ export function activate(context: vscode.ExtensionContext) {
 
   vscode.commands.executeCommand("setContext", "suspenders:isActivated", true);
 
-  const codeLensProvider = new CodeLensProvider();
-  vscode.languages.registerCodeLensProvider({ pattern: "**/BUILD*" }, codeLensProvider);
+  // const codeLensProvider = new CodeLensProvider();
+  // vscode.languages.registerCodeLensProvider({ pattern: "**/BUILD*" }, codeLensProvider);
 
   const testController = vscode.tests.createTestController(
     "suspenders-test-controller",
@@ -75,6 +76,14 @@ export function activate(context: vscode.ExtensionContext) {
   );
   const testsProvider = new TestsProvider(testController);
   vscode.commands.registerCommand("suspenders.tests.refresh", () => testsProvider.refresh());
+
+  // Hardcoding this in as a POC
+  lspClient = createLanguageClient(context);
+  await lspClient.start(); // Throwing away the promise return
+  context.subscriptions.push(lspClient);
+  lspClient.sendNotification(DidChangeConfigurationNotification.type, {
+    settings: null,
+  });
 }
 
 export function deactivate() {
@@ -82,4 +91,82 @@ export function deactivate() {
   //   disposables.forEach((item) => item.dispose());
   // }
   // disposables = [];
+}
+
+// TODO: Grabbed this from https://github.com/microsoft/pyright/blob/496e50f65c8d3aecc43dcbc9b960d633cafe0c94/packages/vscode-pyright/src/extension.ts#L96
+// Spend some time cleaning it up for our purposes
+function createLanguageClient(context: vscode.ExtensionContext) {
+  let serverModule = context.asAbsolutePath(
+    path.join("node_modules", "pyright", "langserver.index.js")
+  );
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6600"] };
+
+  // TODO: Is stdio necessary? I ran into some errors using node ipc, but maybe those were false positives
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.stdio, args: ["--stdio"] },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.stdio,
+      args: ["--stdio"],
+      options: debugOptions,
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    // TODO: this can also be a pattern on BUILD files, but I feel like language-based is more "correct"
+    documentSelector: [{ scheme: "file", language: "pantsbuild" }],
+    middleware: {
+      workspace: {
+        configuration: async (
+          params: ConfigurationParams,
+          token: CancellationToken,
+          next: ConfigurationRequest.HandlerSignature
+        ) => {
+          let result = next(params, token);
+
+          if (isThenable(result)) {
+            logger.debug("createLanguageClient: Result is Thenable");
+            result = await result;
+          }
+          logger.debug(
+            `createLanguageClient: Params: ${JSON.stringify(params)} - Result: ${JSON.stringify(result)}`
+          );
+
+          if (result instanceof ResponseError) {
+            logger.error(`createLanguageClient: ResponseError: ${JSON.stringify(result)}`);
+            return result;
+          }
+
+          for (const [i, item] of params.items.entries()) {
+            logger.debug(`Entries: ${i} ${JSON.stringify(item)}`);
+
+            // TODO: Hardcoding this as a proof-of-concept, but this should probably be part of Suspenders config
+            if (item.section === "python.analysis") {
+              (result[i] as any).stubPath = "./.pants.d";
+            }
+          }
+
+          return result;
+        },
+      },
+    },
+  };
+
+  return new LanguageClient("PantsBuild LSP Client", serverOptions, clientOptions);
+}
+
+function isThenable<T>(v: any): v is Thenable<T> {
+  return typeof v?.then === "function";
+}
+
+// TODO: Kibosh this - many better ways coming up than this using the new `peek`
+function runGoalOnAllTargets(name: string, cwd?: string): void {
+  logger.show();
+
+  const options: proc.SpawnOptions = {
+    cwd,
+  };
+  const subprocess = proc.spawn(getPantsExecutable(), [name, "::"], options);
+  subprocess.stdout?.on("data", (data) => logger.log(data.toString()));
+  subprocess.stderr?.on("data", (data) => logger.log(data.toString()));
 }
